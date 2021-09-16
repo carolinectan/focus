e742ea5137006b7aa730b965b2e367c46d3f3596
Caroline Tan <carolinectan3@gmail.com>
2021-09-16 00:22:32 -0400

    Add welcome index test

---
+++ spec/features/welcome_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'welcome page', type: :feature do
+    it 'has a button' do
+        visit '/'
+
+        expect(page).to have_content('Focus')
+        expect(page).to have_button('Discover Images')
+
+        click_on 'Discover Images'
+
+        expect(current_path).to eq('/discover')
+    end
+end
