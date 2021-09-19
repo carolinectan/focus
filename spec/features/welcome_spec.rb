f66ac2ca21616eeabb5524e932ac286a6dbae4f5
Caroline Tan <carolinectan3@gmail.com>
2021-09-19 17:56:14 -0400

    Fix indentation; refactor with path helpers

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'welcome page', type: :feature do
-    it 'has a button' do
-        visit '/'
+  it 'has a button to go to the discover page' do
+    visit root_path
 
-        expect(page).to have_content('Focus')
-        expect(page).to have_button('Discover Images')
+    expect(page).to have_content('Focus')
+    expect(page).to have_button('Discover Images')
 
-        click_on 'Discover Images'
+    click_on 'Discover Images'
 
-        expect(current_path).to eq('/discover')
-    end
+    expect(current_path).to eq(discover_index_path)
+  end
 end
