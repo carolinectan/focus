4505ac549dc718f3c5b7e4de43bb318893ac704d
Caroline Tan <carolinectan3@gmail.com>
2021-09-20 04:08:05 -0400

    Add by_title image action

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,4 +2,8 @@ class ImagesController < ApplicationController
   def index
     @images = ImageFacade.find_images(params[:subject])
   end
+
+  def by_title
+    @images = ImageFacade.find_images(params[:title])
+  end
 end
