d30a9f8bdb115662a494fcc2877641779c01c7f0
Caroline Tan <carolinectan3@gmail.com>
2021-09-20 04:20:34 -0400

    Remove empty line

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'welcome#index'
-
   get '/discover', to: 'discover#index'
   get '/images', to: 'images#index'
   get '/images/by_title', to: 'images#by_title'
