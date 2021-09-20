e0313883759834e7cb9a348d3ff73987f1ec18c2
Caroline Tan <carolinectan3@gmail.com>
2021-09-20 01:48:25 -0400

    Add title and photographer attributes to image poro

--- app/poros/image.rb
+++ app/poros/image.rb
@@ -1,8 +1,10 @@
 class Image
-  attr_reader :id, :url
+  attr_reader :id, :url, :title, :photographer
 
   def initialize(info)
     @id = info[:id]
     @url = info[:src][:medium]
+    @title = info[:url].sub("https://www.pexels.com/photo/", "").sub("/", "")
+    @photographer = info[:photographer]
   end
 end
