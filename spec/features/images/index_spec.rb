3e92c50f5fcd1b78a50d5733bbfa0d2858f28ce1
Caroline Tan <carolinectan3@gmail.com>
2021-09-20 00:09:01 -0400

    Add images index spec

--- spec/features/images/index_spec.rb
+++ spec/features/images/index_spec.rb
@@ -1,13 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'images index page', type: :feature do
-  it 'displays images search results' do
+  it 'displays images search results with up to max 80 results' do
     visit '/discover'
 
     fill_in :subject, with: 'ocean'
     click_on 'Search'
 
     expect(current_path).to eq('/images')
-    expect(page).to have_content('Image Results')
+    expect(page).to have_content('Image Search Results for "ocean"')
+    expect(page).to have_css('img', :count => 80)
+
+    within '#image-id-189349' do
+      expect(page).to have_css('img', :count => 1)
+    end
   end
 end
