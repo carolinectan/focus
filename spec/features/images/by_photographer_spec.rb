f1fcf5920054341b353036bea194e9924998d427
Caroline Tan <carolinectan3@gmail.com>
2021-09-20 04:16:47 -0400

    Add search by image photographer test

---
+++ spec/features/images/by_photographer_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'images by photographer page', type: :feature do
+  it 'displays images that have a similar photographer name' do
+    visit '/discover'
+
+    fill_in :subject, with: 'ocean'
+    click_on 'Search'
+
+    within '#image-id-189349' do
+      click_on('Similar Images by Photographer')
+    end
+
+    expect(current_path).to eq('/images/by_photographer')
+    expect(page).to have_content('Similar Images by Photographer Results')
+
+    expect(page).to have_content('Searched for "Sebastian Voortman"')
+    expect(page).to have_css('img', :count => 40)
+  end
+end
