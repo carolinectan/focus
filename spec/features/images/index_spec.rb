fc5a83296f7ab55b66727c3ee157e18e97807165
Caroline Tan <carolinectan3@gmail.com>
2021-09-20 04:05:53 -0400

    Add test for search by title and photogapher buttons

--- spec/features/images/index_spec.rb
+++ spec/features/images/index_spec.rb
@@ -11,9 +11,18 @@ RSpec.describe 'images index page', type: :feature do
     expect(page).to have_content('Image Search Results')
     expect(page).to have_content('Searched for "ocean"')
     expect(page).to have_css('img', :count => 80)
+  end
+
+  it 'displays buttons to search for images by title or photographer' do
+    visit '/discover'
+
+    fill_in :subject, with: 'ocean'
+    click_on 'Search'
 
     within '#image-id-189349' do
       expect(page).to have_css('img', :count => 1)
+      expect(page).to have_button('Similar Images by Image Title')
+      expect(page).to have_button('Similar Images by Photographer')
     end
   end
 end
