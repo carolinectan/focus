7145f3cfde99feda69b110aace4d9399cc06e159
Caroline Tan <carolinectan3@gmail.com>
2021-09-20 09:54:01 -0400

    Refactor; update puma

--- spec/facades/image_facade_spec.rb
+++ spec/facades/image_facade_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe ImageFacade do
         images = ImageFacade.find_images(query)
 
         expect(images).to be_an Array
-        expect(images.first).to be_a Image
+        expect(images.first).to be_an Image
         expect(images.size).to eq(80)
       end
     end
